32f15aed511d53ee26dd757bb301290d2e2d4434
Abdallahman Habyarimana <habdallah005@gmail.com>
2022-11-01 21:48:42 -0400

    create add to cart and checkout page

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import { Routes, Route } from "react-router-dom"
 import Navigation from './pages/navigation/navigation.component';
 import Authentication from './pages/authentication/authentication.component';
 import Shop from './pages/shop/shop.component';
+import Checkout from './pages/checkout/checkout.component';
 
 
 
@@ -16,6 +17,7 @@ const App = () => {
       <Route path="/" element={<Navigation />}>
         <Route index  element={ <Home />} />
         <Route path="shop" element={ <Shop />} /> 
+        <Route path="checkout" element={<Checkout />} />
         <Route path="auth" element= { <Authentication />} />
       </Route>
     </Routes>
--- client/src/components/cart-dropdown/cart-dropdown.component.jsx
+++ client/src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,4 +1,5 @@
 import React, { useContext } from 'react'
+import { useNavigate } from 'react-router-dom';
 import { CartContext } from '../../contexts/cart.context';
 import Button from '../botton/button.component';
 import CartItem from '../cart-item/cart-item.component';
@@ -6,7 +7,15 @@ import CartItem from '../cart-item/cart-item.component';
 import "./cart-dropdown.styles.scss"
 
 const CartDropDown = () => {
-  const { cartItems } = useContext(CartContext)
+  const { cartItems, setIsCartOpen, isCartOpen } = useContext(CartContext);
+  const navigate = useNavigate()
+
+  const handleCheckOut = () => {
+    const path = "checkout";
+    navigate(path)
+    setIsCartOpen(!isCartOpen)
+  }
+
   return (
     <div className="cart-dropdown-container">
         <div className="cart-items">
@@ -14,7 +23,7 @@ const CartDropDown = () => {
             cartItems.map(item => <CartItem key={item.id} cartItem={item} />)
           }
         </div>
-        <Button>Go to Checkout</Button>
+        <Button onClick={handleCheckOut}>Go to Checkout</Button>
     </div>
   )
 }
