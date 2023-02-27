37c8ac92b7af68a454c5ba423b8c98566ab36f4b
Abdallahman Habyarimana <habdallah005@gmail.com>
2023-02-27 10:58:30 -0500

    implement fetch Categories with redux-saga

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -3,7 +3,8 @@ import rootReducer from "./root-reducer";
 import storage from 'redux-persist/lib/storage'
 import { persistStore, persistReducer } from "redux-persist";
 import logger from "react-logger";
-import thunk from "redux-thunk"
+import { rootSaga } from "./root-saga";
+import createSagaMiddleware from "@redux-saga/core";
 
 const persistConfig = {
     key: 'root',
@@ -11,9 +12,11 @@ const persistConfig = {
     whitelist: ['cart']
 }
 
+const sagaMiddleware = createSagaMiddleware();
+
 const persistedReducer = persistReducer(persistConfig, rootReducer)
 
-const middleWares = [process.env.NODE_ENV !== 'development' && logger, thunk].filter(Boolean)
+const middleWares = [process.env.NODE_ENV !== 'development' && logger, sagaMiddleware].filter(Boolean)
 
 const composedEnhancer = (process.env.NODE_ENV !== 'production' && window && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose
 
@@ -24,6 +27,8 @@ export const store = createStore(
      undefined, 
      composedEnhancers
 )
+
+sagaMiddleware.run(rootSaga);
 
 export const persistor = persistStore(store)
 
